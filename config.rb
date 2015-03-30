824ace229c00f7ff180a8f502aa43e5080d2ed01
andy lirt <andrew.lirot@jwt.com>
2015-03-30 17:29:31 -0400

    newfil

--- config.rb
+++
@@ -1,7 +0,0 @@
-require  'susy'
-require 'sassy-buttons'
-css_dir = 'css'
-sass_dir = 'sass'
-javascript_dir = 'js'
-output_style = :nested
-images_dir = "images"
--- docs/FUNCTIONAL.md
+++
@@ -1,1 +0,0 @@
-function desing stuff
